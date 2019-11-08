286f57a125096f345ba6255721a875a9abd20710
juwanpetty <jchpetty@gmail.com>
2019-11-08 14:16:35 -0500

    Dispatching actions

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -5,7 +5,34 @@ const initialState = {
 };
 
 const store = createStore((state = initialState, action) => {
-  return state;
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        count: state.count + 1
+      };
+    case "DECREMENT":
+      return {
+        count: state.count - 1
+      };
+    case "RESET":
+      return {
+        count: 0
+      };
+    default:
+      return state;
+  }
 });
+
+console.log(store.getState());
+
+// TODO: Add Increment Action
+store.dispatch({ type: "INCREMENT" });
+
+// TODO: Add Decrement Action
+store.dispatch({ type: "DECREMENT" });
+store.dispatch({ type: "DECREMENT" });
+
+// TODO: Add Reset Action
+// store.dispatch({ type: "RESET" });
 
 console.log(store.getState());
